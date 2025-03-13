65184807d5014be3dbbdf350c711a0d2d7e8f6ec
Your GitHub Username <thehanginghouse@gmail.com>
2025-03-13 12:59:13 +0400

    Update index.js

--- src/components/StaffLinks/index.js
+++ src/components/StaffLinks/index.js
@@ -26,9 +26,9 @@ const Home = () => {
                 <div className='green' style={{display: 'flex', gap: '20px', justifyContent: 'center', alignItems: 'center', marginBottom: '50px'}}>
                     <div className='holder' style={{display: 'flex', gap: '20px'}}>
                         {/* eslint-disable-next-line */}
-                        <a rel="noreferrer" target="_blank" href="https://hanginghouse.keka.com/">
+                        <a rel="noreferrer" target="_blank" href="https://thehanginghouse.keka.com/">
                             <div className="ButtonDiv2">
-                                <p>WebHR</p>
+                                <p>Keka</p>
                             </div>
                         </a> 
                         
